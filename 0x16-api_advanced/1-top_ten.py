3df8c7df67ba692cb81607de80f6177f6312ad7e
webmasters2019 <lively222@live.com>
2022-04-26 20:00:46 +0300

    Task 1

---
+++ 0x16-api_advanced/1-top_ten.py
@@ -0,0 +1,24 @@
+#!/usr/bin/python3
+
+"""
+Function that queries the Reddit API and prints
+the titles of the first 10 hot posts listed
+for a given subreddit.
+"""
+
+
+def top_ten(subreddit):
+    """
+    This function returns the top 10 hot posts
+    """
+    import requests
+
+    sub_info = requests.get("https//www.reddit.com/r/{}/hot.json?limit=10"
+                            .format(subreddit),
+                            headers={"User-Agent": "My-User-Agent"},
+                            allow_redirects=False)
+    if sub_info.status_code >= 300:
+        print('None')
+    else:
+        [print(child.get("data").get("title"))
+         for child in sub_info.json().get("data").get("children")]
