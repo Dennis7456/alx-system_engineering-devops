e7229ab9d3093ac152932eec914bdc39a5ef2169
webmasters2019 <lively222@live.com>
2022-04-26 19:23:24 +0300

    Task 0

---
+++ 0x16-api_advanced/0-subs.py
@@ -0,0 +1,20 @@
+#!/usr/bin/python3
+
+"""
+A function that queries the Redit API and returns the number of
+total subscribers for a given subreddit.
+"""
+
+
+def number_of_subscribers(subreddit):
+    import requests
+
+    sub_info = requests.get("https://www.reddit.com/r/{}/about.json"
+                            .format(subreddit),
+                            headers={"User-Agent": "My-User-Agent"},
+                            allow_redirects=False)
+
+    if sub_info.status_code >= 300:
+        return 0
+
+    return sub_info.json().get('data').get('subscribers')
